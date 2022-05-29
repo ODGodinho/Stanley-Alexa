cf78cc4f18c0db979003b85d05611447265e1126
Dragons Gamers <DragonsGamers@hotmail.com.br>
2022-05-29 15:59:55 -0300

    fix(Deploy): :green_heart: fix bug package.json
    change main in package.json after build

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 yarn tsc
 
-cp -avr ./package.json ./dist/lambda/
+rm -rf ./dist/lambda/package.json
+node -e '
+    const package = require("./package.json");
+    package.main = "index.js";
+    delete package.devDependencies;
+    console.log(JSON.stringify(package, null, 2))
+' >> ./dist/lambda/package.json
 cp -avr ./.ask/ ./dist/
 cp -avr ./skill-package/ ./dist/
 cp -avr ./ask-resources.json ./dist/ask-resources.json
